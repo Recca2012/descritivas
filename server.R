9ed0b824ab50a3733a5c50c12ba7e7832c50359e
Mauricio Fernandes <mauriciofernandes1991@gmail.com>
2014-05-30 13:18:44 -0300

    first commit

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
           "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 u <- "https://raw.githubusercontent.com/Recca2012/descritivas/master/descritivias.R"
+require(RCurl)
 script <- getURL(u, ssl.verifypeer = FALSE)
 eval(parse(text = script))
 load("data/dados.RData",envir=.GlobalEnv)
